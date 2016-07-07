ffe26072b6da8c8cca8eb533561920258d5c5ffd
flowmemo <flowmemo@outlook.com>
2016-07-07 20:47:30 +0800

    reduce

--- client.js
+++ client.js
@@ -1,38 +1,13 @@
 'use strict'
-// import test from 'ava'
 const net = require('net')
-// const Promise = require('bluebird')
 require('./server.js')
-
-
-var a = {
-  messageid: 1,
-  time: '123343242',
-  content: {
-    foo: 'bar',
-    text: 'gitjlffas lsjl fjlsdjafljljfrji orjioj82'
-  }
-}
 
 let client = net.connect({ port: 5556 }, () => {
   console.log('client event')
-  // console.log(testcase.obj.method)
   client.write('hello')
   client.end()
-  // console.log('client connect end')
 })
 
-// test(async t => {
-// setTimeout(async t => {
-  /*
-  var p1 = Promise.resolve('client 0')
-  p1.then(() => console.log('client 1'))
-  .then(() => console.log('client 2'))
-  */
-// })
-// }, 0)
-
-// test(t => t.pass())
 
 
 
--- server.js
+++ server.js
@@ -1,40 +1,8 @@
 'use strict'
-
 const net = require('net')
-// const Promise = require('bluebird')
-const p = Promise.resolve(1)
-
-
-const server = net.createServer({ allowHalfOpen: false }, sock => {
-  console.log('server event')
-  sock.on('data', data => {
-    // console.log('DATA ' + sock.remoteAddress + ': ' + data)
-    /*
-    try {
-      let mes = (data.toString())
-      // console.log(mes)
-      // two promise
-      p
-        .then(data => {
-          console.log('server 1')
-          return 'server 2'
-        })
-        .then(data => console.log(data))
-        .catch(err => console.error((err)))
-    } catch (err) {
-      console.error((err))
-      return
-    }
-    */
-  })
-
-  sock.on('end', () => {
-     // console.log(('client disconnected'))
-  })
-})
 
 const PORT = 5556
 const HOST = '0.0.0.0'
-server.listen(PORT, HOST, () => {
-  console.log(`serve on port ${HOST}:${PORT}`)
-})
+const server = net.createServer(sock => console.log('server event'))
+
+server.listen(PORT, HOST)
