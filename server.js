60ae6ad9a1c37f50fc54ba99c09a3d1f6de43dfb
flowmemo <flowmemo@outlook.com>
2016-07-07 20:55:23 +0800

    reduce

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const net = require('net')
-
-const PORT = 5556
-const HOST = '0.0.0.0'
-const server = net.createServer(sock => console.log('server event'))
-
-server.listen(PORT, HOST)
